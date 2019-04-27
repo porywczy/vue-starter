4a54b3d1c484306e30be2a413aa57962da0d6f10
student6 <student6@pcoip.ki.agh.edu.pl>
2019-04-27 15:45:56 +0200

    next_4 commit

--- src/LoggedInPage.vue
+++ src/LoggedInPage.vue
@@ -2,11 +2,29 @@
     <div>
         <h2>Zalogowany jako {{username}}</h2>
         <button @click="$emit('logout')">Wyloguj</button>
+        <h2>Spotkania</h2>
+        <button @click="addNewMeeting()">Dodaj nowe spotkanie</button>
+        <ol>
+            <li v-for="meeting in meetings">
+                {{ meeting }}
+            </li>
+        </ol>
     </div>
+
 </template>
 
 <script>
     export default {
-      props: ['username']
+      props: ['username'],
+        data() {
+          return {
+              meetings: ['MWO', 'TO', 'INNE']
+          }
+        },
+        methods: {
+            addNewMeeting(meeting) {
+                this.meetings.push('nowe');
+            }
+        }
     }
 </script>
